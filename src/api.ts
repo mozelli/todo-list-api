77a9c7985bc933021337492dbad8d74df4b07154
João Mozelli Neto <joaomozelli@hotmail.com>
2023-03-09 12:57:19 -0300

    create tasks route

--- src/api.ts
+++ src/api.ts
@@ -1,1 +1,11 @@
-console.log("Hello World");
+require('dotenv/config');
+import express from "express";
+import { routes } from "./routes";
+
+
+const app = express();
+app.use(express.json());
+
+app.use("/tasks", routes);
+
+app.listen(process.env.PORT, () => console.log(`Server is running on http://localhost:${process.env.PORT}`));
